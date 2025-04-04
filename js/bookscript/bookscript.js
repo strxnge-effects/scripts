a7dbd5f9f241af73b84e43852084cc66f50f3690
strxnge-effects <thea_apianae@gmail.com>
2025-04-04 11:59:28 -0700

    create js folder
    bookscript: get title + author of book from status cafe
    arttags:    generate tumblr art tags

---
+++ js/bookscript/bookscript.js
@@ -0,0 +1,46 @@
+import {markdownTable} from 'markdown-table';
+import {marked} from 'marked';
+
+
+fetch("https://status.cafe/users/transneptunianobject/status.json")
+  .then( r => r.json() )
+  .then( r => {
+    if (!r.content.length) {
+      console.log("no status (how)");
+    }
+
+
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    var myObj = {
+      "title": "",
+      "author": "",
+      "date": ""
+    };
+
+    let coolArray = r.content.split(": ")[1].split(" by ");
+
+    myObj.title = coolArray[0];
+    myObj.author = coolArray[1];
+
+    if (process.argv[2] == "-d") {
+      const d = new Date(process.argv[3]);
+      myObj.date = `${months[d.getMonth()]} ${d.getDate()}`;
+
+    } else {
+      const d = new Date();
+      myObj.date = `${months[d.getMonth()]} ${d.getDate()}`;
+    }
+
+
+    // > markdown table
+    let mdOutput = markdownTable([
+      ["title", "author", "date"],
+      [myObj.title, myObj.author, myObj.date]
+    ]);
+
+    const html = marked.parse(mdOutput);
+
+    console.log(html)
+    console.log(mdOutput);
+});
