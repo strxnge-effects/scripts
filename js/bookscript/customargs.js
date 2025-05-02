c803eabeb68fceca6db012d21a8e17d0315aa85a
strxnge-effects <thea_apianae@gmail.com>
2025-05-01 22:40:31 -0700

    reindent customargs.js

--- js/bookscript/customargs.js
+++ js/bookscript/customargs.js
@@ -6,41 +6,41 @@ import prompt from "prompt";
 prompt.start();
 
 prompt.get(["title", "author", "date"], function (err, result) {
-    if (err) { return onErr(err); }
+  if (err) { return onErr(err); }
 
 
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    var myObj = {
-      "title": result.title,
-      "author": result.author,
-      "date": ""
-    };
-
-
-    if (result.date) {
-      const d = new Date(result.date);
-      myObj.date = `${months[d.getMonth()]} ${d.getDate()}`;
-
-    } else {
-      const d = new Date();
-      myObj.date = `${months[d.getMonth()]} ${d.getDate()}`;
-    }
+  var myObj = {
+    "title": result.title,
+    "author": result.author,
+    "date": ""
+  };
 
 
-    // > markdown table
-    let mdOutput = markdownTable([
-      ["title", "author", "date"],
-      [myObj.title, myObj.author, myObj.date]
-    ]);
+  if (result.date) {
+    const d = new Date(result.date);
+    myObj.date = `${months[d.getMonth()]} ${d.getDate()}`;
 
-    const html = marked.parse(mdOutput);
+  } else {
+    const d = new Date();
+    myObj.date = `${months[d.getMonth()]} ${d.getDate()}`;
+  }
 
-    console.log(html)
-    console.log(mdOutput);
+
+  // > markdown table
+  let mdOutput = markdownTable([
+    ["title", "author", "date"],
+    [myObj.title, myObj.author, myObj.date]
+  ]);
+
+  const html = marked.parse(mdOutput);
+
+  console.log(html)
+  console.log(mdOutput);
 });
 
 function onErr(err) {
-    console.log(err);
-    return 1;
+  console.log(err);
+  return 1;
 }
