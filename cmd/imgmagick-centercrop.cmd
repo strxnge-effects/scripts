925b96101b39f6d410def0b7a6dd2883bf6fd110
Caritina Evans <thea_apianae@proton.me>
2026-02-08 20:10:12 -0800

    cmd/imgmagick-centercrop: use 1 input val for both width and height, rather than 2 separate prompts
    since that what i use it for anyway

--- cmd/imgmagick-centercrop.cmd
+++ cmd/imgmagick-centercrop.cmd
@@ -1,5 +1,4 @@
 @echo off
-set /p "W=width: "
-set /p "H=height: "
+set /p "W=width / height: "
 
-magick %1 -gravity Center -crop %W%x%H%+0+0 +repage "%~p1%~n1_centercrop%~x1"
+magick %1 -gravity Center -crop %W%x%W%+0+0 +repage "%~p1%~n1_centercrop%~x1"
